a4630010824c5d57fffcddc90e6894eccdcbfe5d
Maria <mariacutipa.gonzales@gmail.com>
2017-04-25 18:54:49 -0500

    fusionForm

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,8 @@ function showDivs(n) {
 }
 
 
- function init(){
+/*GOOGLE MAPS*/
+function init(){
         var mapOption = {
          center: new google.maps.LatLng(-12.1189618,-77.040862),
          zoom: 5,
@@ -37,3 +38,106 @@ function showDivs(n) {
  
  }
 google.maps.event.addDomListener(window, 'load', init);
+
+/*MODAL GALLERY*/
+var modal=document.getElementById("modal");/*Modal fondo*/
+var close=document.getElementById("close");/*Button Close*/
+var img = document.getElementById("gallery");/*Imagen alimentador*/
+var contentImage=document.getElementById("viewImage");/*Contenedo de la imagen modal*/
+var classImage = document.getElementsByClassName("div-image");/*clase imagenes*/
+img.addEventListener("click",function(event){
+    if (event.target.tagName == "IMG"){
+      modal.style.display = "block";
+      contentImage.src = event.target.src;
+    }
+});
+
+close.addEventListener("click",function(){
+  modal.style.display = "none";
+});
+
+/*JS CONTACTANOS*/
+// Validacion de formulario
+enviar.onclick = function() {
+  var nombre = document.getElementById("name");
+  var apellido = document.getElementById("lastname");
+  var correo = document.getElementById("input-email");
+  var phone = document.getElementById("telefono");
+  var msje = document.getElementById("msj");
+  //Ingresar los campos necesarios
+  if (nombre.value == "" || apellido.value == "" || correo.value == "" || phone.value == ""){
+    msje.innerText = "*Verifica que datos obligatorios estan incompletos";
+  }
+  //caracteres expresion regular
+  var crtCantidad = /^\s+|\s+$/;
+  var letras = /[a-zA-Z]/;
+  var crtNumero = /[0-9]/;
+  validaNombre();
+  //valido el nombre
+  function validaNombre(){
+    var msg = document.getElementById("msja");
+    if (nombre.value.length < 3 || crtCantidad.test(nombre.value)){
+      msg.innerText = "Tiene que ingresar su nombre";
+      return nombre.focus();
+    } else if (crtNumero.test(nombre.value) || !(letras.test(nombre.value))){
+      msg.innerText = "Solo se permiten letras";
+      return nombre.focus();
+    } else if (!(/[^a-z]+([a-z]{1,})/.test(nombre.value))){
+      msg.innerText = "Su nombre debe empezar con mayúscula";
+      return nombre.focus();
+    } else{
+      msg.innerText = "";
+      msg.style.display = "none";
+      validaApellido();
+    }
+  }
+  //valido el apellido
+  function validaApellido(){
+    var msg = document.getElementById("msjb");
+    if (apellido.value.length < 3 || crtCantidad.test(apellido.value)){
+      msg.innerText = "Tiene que ingresar su apellido";
+      return apellido.focus();
+    } else if (crtNumero.test(apellido.value) || !(letras.test(apellido.value))){
+      msg.innerText = "Solo se permite letras";
+      return apellido.focus();
+    } else if (!(/[^a-z]+([a-z]{1,})/.test(apellido.value))){
+      msg.innerText = "Su apellido debe empezar con mayúscula";
+      return apellido.focus();
+    } else{
+      msg.innerText = ""
+      msg.style.display = "none";
+      validaEmail();
+    }
+  }
+  //valido el correo
+  function validaEmail(){
+    var msg = document.getElementById("msjc")
+    var crt = /\S+@\+\S+.\+S+/;
+    var crt2= /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+    if (!crt.test(correo.value) && correo.value.length < 10){
+      msg.innerText = "Ingrese el formato correcto del correo";
+      return correo.focus();
+    } else if (!(crt2.test(correo.value))){
+      msg.innerText = "Correo no válido \n ej: abcd@lab.co";
+      return correo.focus();
+    } else{
+      msg.innerText = ""
+      msg.style.display = "none";
+      validaPhone();
+    }
+  }
+  //valido la telefono
+  function validaPhone(){
+    var msg = document.getElementById("msjd");
+    var expresion = /^9[\d]{8}$/;
+    if (isNaN(phone.value) || !expresion.test(phone.value)){
+      msg.innerText = "Debe ingresar un número con el formato correcto \n ej: 987654321";
+      return phone.focus();
+    } else{
+      msg.innerText = "";
+      msg.style.display = "none";
+      msje.style.color = "rgb(241, 27, 97)";
+      msje.innerText = "Gracias por su visita :)";
+    }
+  }
+}
